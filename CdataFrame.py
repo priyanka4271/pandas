003578b884d5bd404fe0e1ac09fbde24e9cbee50
priyanka4271 <ps9673932@gmail.com>
2025-04-04 12:26:21 +0530

    add new file

---
+++ CdataFrame.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from pandas import DataFrame ,Series
+list1={'Name':['John','Jane','Mike','Emma','Sarah'],'Age':[25,30,28,32,27],'id':['220','330','440','550','660']}
+df = pd.DataFrame(list1)
+print(df)   
